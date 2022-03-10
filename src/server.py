ed155e5fa976137c45f1aa90f0a3e1c0a57abc16
Michael <michaelobineche99@gmail.com>
2022-03-10 01:44:07 +0000

    two clients now able to message each other

--- src/server.py
+++ src/server.py
@@ -15,33 +15,65 @@ PORT = 5555
 
 class ThreadedClient(threading.Thread):
     """Threadclient class for each client that connects"""
+
+    clients = {
+        "1": None,
+        "2": None,
+    }
+
     def __init__(self, client):
         threading.Thread.__init__(
             self,
         )
         self.event = threading.Event()
         self.client = client
+        self.client_id = self.get_id()
+
+        self.client.send(str.encode(self.client_id))
+        ThreadedClient.clients[self.client_id] = self.client
 
     def run(self):
-        print(f"{self.client.getsockname()[0]} has connected")
+        print(f"{self.client_id}:{self.client.getsockname()[0]} has connected")
         while not self.event.is_set():
             readable, _, _ = select.select([self.client], [], [], 2)
             for obj in readable:
                 if obj is self.client:
                     data = self.client.recv(1024)
                     if not data:
-                        print(f"{self.client.getsockname()[0]} has disconnected")
-                        self.client.close()
-                        self.event.set()
+                        self.set_event()
                         break
-                    print(f"Received message from {data.decode()}")
-    
+
+                    sender_id, message = data.decode().split(":")
+                    sender_id = int(sender_id)
+
+                    reciever_id = None
+                    if sender_id == 1:
+                        reciever_id = ThreadedClient.clients.get("2")
+                    if sender_id == 2:
+                        reciever_id = ThreadedClient.clients.get("1")
+
+                    if reciever_id is None:
+                        self.client.send(str.encode("No end user is connected"))
+                        continue
+                    reciever_id.send(str.encode(message))
+
+        print(f"{self.client.getsockname()[0]} has disconnected")
+        ThreadedClient.clients[self.client_id] = None
+
+    def get_id(self):
+        """Return an ID for the client"""
+        if ThreadedClient.clients.get("1") is None:
+            return "1"
+        return "2"
+
     def set_event(self):
+        """Stop the thread"""
         self.event.set()
 
 
 class Socket:
     """Server class"""
+
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def __init__(self, host, port):
@@ -50,11 +82,11 @@ class Socket:
         self.sock.listen(2)
         self.running_threads = []
         self.connections = 0
-    
+
     def check_running_threads(self):
         """Check number of threaded clients"""
-        self.connections =  threading.active_count() - 1
-            
+        self.connections = threading.active_count() - 1
+
     def run(self):
         """Entry to point to start server"""
         print("Server is running!")
@@ -65,7 +97,7 @@ class Socket:
                 for obj in readable:
                     if obj is self.sock:
                         client, _ = self.sock.accept()  # Blocking
-                        if self.connections == 1:  # Max connections
+                        if self.connections == 2:  # Max connections
                             client.close()  # Close client instantly
                             continue
 
