7d7ff4cc0e842784d5685fbd55b11cc95778243e
Michael <michaelobineche99@gmail.com>
2022-03-13 16:33:53 +0000

    removed Move class & added more type hinting

--- src/game.py
+++ src/game.py
@@ -1,5 +1,5 @@
 """Game object"""
-from typing import Union
+from typing import Union, Tuple, List, Dict
 import numpy as np
 
 class GameEngine:
@@ -67,7 +67,7 @@ class GameEngine:
             return True
         return False
 
-    def check_has_pawn_moved(self,current_row: int, piece_color: str) -> bool:
+    def has_pawn_moved(self,current_row: int, piece_color: str) -> bool:
         """Given a row and color return whether a pawn has moved"""
         if current_row == 6 and piece_color == "w":
             return False
@@ -88,18 +88,12 @@ class GameEngine:
         self.black_moves.clear()
 
         # Loop board and get moves for each pieace
-        for index, chess_square in np.ndenumerate(self.board):
+        for index, chess_square in np.ndenumerate(self.board): # Type: tuple(int,int) , str
             if chess_square != "--":
 
                 array: list = []
-                piece_color: str
-                piece_type: str
-                piece_color, piece_type = chess_square
-
-                if piece_color == "w":
-                    array = self.white_moves
-                if piece_color == "b":
-                    array = self.black_moves
+                piece_color, piece_type = chess_square # Type: str, str
+                array = self.white_moves if piece_color == "w" else self.black_moves
 
                 if piece_type == "P":  # Pawn
                     self.get_pawn_moves(index, array, chess_square)
@@ -112,18 +106,12 @@ class GameEngine:
     def get_black_moves(self):
         return self.black_moves
 
-    def get_non_pawn_moves(self, index: tuple, array: list, chess_square: str) -> None:
+    def get_non_pawn_moves(self, index: Tuple[int,int], array: List[Dict[list,bool]], chess_square: str) -> None:
         """Generate non-pawn moves here"""
-        row: int
-        col: int
-        row, col = index
         # ---------------
-        piece_color: str
-        piece_type: str
+        row, col = index
         piece_color, piece_type = chess_square
         # -------------------------------------
-        movements: list
-        is_continious: bool
         movements, is_continious = self.get_piece_moves_dict(piece_type)
 
         # Loop through piece movements list
@@ -132,7 +120,7 @@ class GameEngine:
             while self.is_in_bounds(new_row, new_col):  #
                 # Check if the square is empty
                 if self.board[new_row][new_col] == "--":
-                    array.append((f"{col}{row}", f"{new_col}{new_row}"))
+                    array.append((f"{col}{row}",f"{new_col}{new_row}"))
                     if not is_continious:
                         break
                     new_row += add_x
@@ -142,67 +130,32 @@ class GameEngine:
                     if self.board[new_row][new_col][0] == piece_color:
                         break
                     # Collides with enemy piece
-                    array.append((f"{col}{row}", f"{new_col}{new_row}"))
+                    array.append((f"{col}{row}",f"{new_col}{new_row}"))
                     break
 
-    def get_pawn_moves(self, index: tuple, array: list, chess_square: str) -> None:
+    def get_pawn_moves(self, index: Tuple[int,int], array: List[Dict[list,bool]], chess_square: str) -> None:
         """Generate pawn moves"""
-
         # -------------------------------------
-        row: int
-        col: int
         row, col = index
-        # -------------------------------------
-        piece_color: str
-        piece_type: str
         piece_color, piece_type = chess_square
+        direction = -1 if piece_color == "w" else 1
         # -------------------------------------
-        movements: list
         movements, _ = self.get_piece_moves_dict(piece_type)
-
-        # -------------------------------------
-        direction = 0
-        if piece_color == "w":
-            direction = -1
-        if piece_color == "b":
-            direction = 1
 
         # Check if its inbounds
         if self.is_in_bounds(row + direction, col):
+            
+            # One square move
             if self.board[row + direction][col] == "--":  # If empty
-                array.append((f"{col}{row}", f"{col}{row + direction}"))
+                array.append((f"{col}{row}",f"{col}{row + direction}"))
 
                 # Two square move
-                if (
-                    not self.check_has_pawn_moved(row, piece_color)
-                    and self.board[row + (direction * 2)][col] == "--"
-                ):
-                    array.append((f"{col}{row}", f"{col}{row+(direction*2)}"))
-            # Capture
+                if (not self.has_pawn_moved(row, piece_color) and self.board[row + (direction * 2)][col] == "--"):
+                    array.append((f"{col}{row}",f"{col}{row+(direction*2)}"))
+                    
+            # Captures
             for add_y in movements:
                 if 0 <= (col + add_y) <= 7:
                     if self.board[row + direction][col + add_y][0] != "-":
-                        if (
-                            self.board[row + direction][col + add_y][0] != piece_color
-                        ):  # Move up left check
-                            array.append(
-                                (f"{col}{row}", f"{col+ add_y}{row+direction}")
-                            )
-
-
-class Move:
-    """Class that stores info about a move"""
-
-    def __init__(self, start_square, end_square,board):
-        """Each move has a move type Normal | Capture | Castle | EnPassant
-        start_square: (tuple) -> (row,col)
-        end_square: (tuple) -> (row,col)
-        """
-        self.start_square = start_square
-        self.end_square = end_square
-        self.start_row = int(start_square[1])
-        self.start_col = int(start_square[0])
-        self.end_row = int(end_square[1])
-        self.end_col = int(end_square[0])
-        self.piece_moved = board[self.start_row][self.start_col]
-        self.piece_captured = board[self.end_row][self.end_col]
+                        if (self.board[row + direction][col + add_y][0] != piece_color):  # Move up left check
+                            array.append((f"{col}{row}",f"{col+ add_y}{row+direction}"))
