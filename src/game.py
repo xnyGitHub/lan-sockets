62cf71c879e794b82b86eae0b352445db0b95d7a
Michael <michaelobineche99@gmail.com>
2022-04-03 17:51:44 +0100

    linting and re-ordered functions

--- src/game.py
+++ src/game.py
@@ -1,7 +1,6 @@
 """Game object"""
 # type: ignore
-from typing import Tuple, List, Union
-from unittest import result
+from typing import Tuple, List
 import numpy as np
 
 
@@ -16,6 +15,8 @@ class GameEngine:
         self.white_moves: list = []
         self.black_moves: list = []
         self.piece_moves: dict = self.piece_movemovents()
+
+        self.check_status: dict = {}
 
         """Default board constructor"""
         self.board: np.ndarray = np.array(
@@ -123,61 +124,50 @@ class GameEngine:
         elif self.player_turn == "white":
             self.player_turn = "black"
 
-    def get_white_moves(self) -> List[str]:
-        """Return list of white moves"""
-        return self.white_moves
-
-    def get_black_moves(self) -> List[str]:
-        """Return list of black moves"""
-        return self.black_moves
-
     def get_board(self) -> np.ndarray:
         """Return the board"""
         return self.board
+
+    def get_white_moves(self) -> List[str]:
+        """Return list of white moves"""
+        return self.white_moves
 
     def get_move_log(self) -> List[str]:
         """Return the move log"""
         return self.move_log
 
+    def get_black_moves(self) -> List[str]:
+        """Return list of black moves"""
+        return self.black_moves
+
+    def get_check_status(self) -> dict:
+        """Return the check status"""
+        return self.check_status
+
+    def update_check_status(self) -> None:
+        """Check whether either play is in check"""
+        if self.player_turn == "white":
+            white_king_location = self.get_king_location("wK")
+            results = [moves for moves in self.black_moves if white_king_location in moves]
+            if results:
+                self.check_status["king_location"] = white_king_location
+                self.check_status["attacking_pieces"] = results
+            else:
+                self.check_status = {}
+
+        if self.player_turn == "black":
+            black_king_location = self.get_king_location("bK")
+            results = [moves for moves in self.white_moves if black_king_location in moves]
+            if results:
+                self.check_status["king_location"] = black_king_location
+                self.check_status["attacking_pieces"] = results
+            else:
+                self.check_status = {}
+
     def get_king_location(self, king_piece: str) -> str:
         """Return the kings location for the piece passed in"""
         row, col = np.where(self.board == king_piece)
         return f"{col[0]}{row[0]}"
-
-    def get_check_status(self) -> list:
-
-        check_status: dict = {}
-
-        if self.player_turn == 'white':
-            white_king_location = self.get_king_location("wK")
-            results = [moves for moves in self.black_moves if white_king_location in moves]
-            if results:
-                check_status['king_location'] = white_king_location
-                check_status['attacking_pieces'] = results
-
-        if self.player_turn == 'black':
-            black_king_location = self.get_king_location("bK")
-            results = [moves for moves in self.white_moves if black_king_location in moves]
-            if results:
-                check_status['king_location'] = black_king_location
-                check_status['attacking_pieces'] = results
-
-        return check_status
-
-    def piece_movemovents(self) -> dict:
-        """Piece movements helper function"""
-        # pylint: disable=no-self-use
-        # fmt: off
-        map_dict = {
-            "P": {"movements": (1, -1), "continous": False},
-            "R": {"movements": [(1, 0), (0, 1), (-1, 0), (0, -1)], "continous": True},
-            "N": {"movements": [(-2, -1),(-2, 1),(2, -1),(2, 1),(-1, -2),(1, -2),(-1, 2),(1, 2),],"continous": False},
-            "B": {"movements": [(1, 1), (-1, 1), (1, -1), (-1, -1)], "continous": True},
-            "Q": {"movements": [(1, 1),(-1, 1),(1, -1),(-1, -1),(1, 0),(0, 1),(-1, 0),(0, -1),],"continous": True},
-            "K": {"movements": [(1, 1),(-1, 1),(1, -1),(-1, -1),(1, 0),(0, 1),(-1, 0),(0, -1),],"continous": False}
-        }
-        # fmt: on
-        return map_dict
 
     def is_in_bounds(self, new_x: int, new_y: int) -> bool:
         """Check if a set of cords is in-bounds"""
@@ -185,23 +175,6 @@ class GameEngine:
         if 0 <= new_x <= 7 and 0 <= new_y <= 7:
             return True
         return False
-
-    def has_pawn_moved(self, current_row: int, piece_color: str) -> bool:
-        """Given a row and color return whether a pawn has moved"""
-        # pylint: disable=no-self-use
-        if current_row == 6 and piece_color == "w":
-            return False
-        if current_row == 1 and piece_color == "b":
-            return False
-        return True
-
-    def get_piece_moves_dict(self, piece_type: str) -> Tuple[List[Tuple[int, int]], bool]:
-        """Return info: (dict) on ghow a particular piece moves"""
-
-        piece_move_info: dict = self.piece_moves[piece_type]
-        movements: List[Tuple[int, int]] = piece_move_info["movements"]
-        continuous: bool = piece_move_info["continous"]
-        return movements, continuous
 
     def get_moves(self) -> None:
         """Call the functions that will generate all legal moves"""
@@ -217,6 +190,135 @@ class GameEngine:
 
         self.check_castle_rights_for_white()
         self.check_castle_rights_for_black()
+        self.update_check_status()
+
+    def generate_all_moves(self) -> None:
+        """Function that calls get moves"""
+        # Clear each time otherwise we end up with duplicates
+        self.white_moves.clear()
+        self.black_moves.clear()
+        index: Tuple[int, int]
+        chess_square: str
+        piece_color: str
+        piece_type: str
+        # Loop board and get moves for each pieace
+        for index, chess_square in np.ndenumerate(self.board):  # type: ignore
+            if chess_square != "--":
+
+                array: list = []
+                piece_color, piece_type = chess_square  # type: ignore
+                array = self.white_moves if piece_color == "w" else self.black_moves
+
+                if piece_type == "P":  # Pawn
+                    self.get_pawn_moves(index, array, chess_square)
+                else:
+                    self.get_non_pawn_moves(index, array, chess_square)
+
+    def get_non_pawn_moves(self, index: Tuple[int, int], array: list, chess_square: str) -> None:
+        """Generate non-pawn moves here"""
+        # ---------------
+        row: int
+        col: int
+        piece_color: str
+        piece_type: str
+        row, col = index
+        piece_color, piece_type = chess_square  # type: ignore
+        # -------------------------------------
+        movements, is_continious = self.get_piece_moves_dict(piece_type)
+
+        for add_x, add_y in movements:  # Loop through piece movements list
+            new_row, new_col = row + add_x, col + add_y  # Get new starting pos
+            while self.is_in_bounds(new_row, new_col):
+
+                # Check if the square is empty
+                if self.board[new_row][new_col] == "--":
+                    array.append(f"{col}{row}:{new_col}{new_row}:N")
+                    if not is_continious:  # If piece type doesn't continuously move e.g Knight, Pawn, King etc..
+                        break
+                    new_row += add_x
+                    new_col += add_y
+                else:
+                    # Collides with team piece
+                    if self.board[new_row][new_col][0] == piece_color:
+                        break
+                    # Collides with enemy piece
+                    array.append(f"{col}{row}:{new_col}{new_row}:T")
+                    break
+
+    def get_pawn_moves(self, index: Tuple[int, int], array: list, chess_square: str) -> None:
+        """Generate pawn moves"""
+        # -------------------------------------
+        row: int
+        col: int
+        direction: int
+        piece_color: str
+        piece_type: str
+        row, col = index
+        piece_color, piece_type = chess_square  # type: ignore
+        direction = -1 if piece_color == "w" else 1
+        # -------------------------------------
+        movements, _ = self.get_piece_moves_dict(piece_type)
+
+        # Check if its inbounds
+        if self.is_in_bounds(row + direction, col):
+
+            # One square move
+            if self.board[row + direction][col] == "--":  # If empty
+                array.append(f"{col}{row}:{col}{row + direction}:N")
+
+                # Two square move
+                if (
+                    not self.has_pawn_moved(row, piece_color) and self.board[row + (direction * 2)][col] == "--"
+                ):  # If its empty and pawn hasn't moved
+                    array.append(f"{col}{row}:{col}{row+(direction*2)}:N")
+
+            # Captures
+            for add_y in movements:
+                new_y = col + add_y  # type: ignore
+                if 0 <= new_y <= 7:  # In-bounds
+                    if self.board[row + direction][new_y][0] != "-":  # Not empty square
+                        if self.board[row + direction][new_y][0] != piece_color:  # Collides with enemy
+                            array.append(f"{col}{row}:{new_y}{row+direction}:T")
+
+    def has_pawn_moved(self, current_row: int, piece_color: str) -> bool:
+        """Given a row and color return whether a pawn has moved"""
+        # pylint: disable=no-self-use
+        if current_row == 6 and piece_color == "w":
+            return False
+        if current_row == 1 and piece_color == "b":
+            return False
+        return True
+
+    def filter_invalid_moves(self) -> None:
+        """Remove illegal moves"""
+        black_invalid = []  # This will hold in valid moves
+        white_invalid = []
+
+        if self.player_turn == "white":
+            for white_move in self.white_moves:
+                self.make_move(white_move)
+                self.generate_all_moves()
+                for opponent_move in self.black_moves:
+                    white_king_location = self.get_king_location("wK")
+                    if white_king_location in opponent_move:
+                        white_invalid.append(white_move)
+                        break
+                self.undo_move()
+                self.generate_all_moves()
+        else:
+            for black_move in self.black_moves:
+                self.make_move(black_move)
+                self.generate_all_moves()
+                for opponent_move in self.white_moves:
+                    black_king_location = self.get_king_location("bK")
+                    if black_king_location in opponent_move:
+                        black_invalid.append(black_move)
+                        break
+                self.undo_move()
+                self.generate_all_moves()
+
+        self.black_moves = list(set(self.black_moves) - set(black_invalid))
+        self.white_moves = list(set(self.white_moves) - set(white_invalid))
 
     def check_castle_rights_for_white(self) -> None:
         """Check if white can castle"""
@@ -324,121 +426,25 @@ class GameEngine:
         if queen_side:
             self.black_moves.append("40:60:70:50:C")
 
-    def generate_all_moves(self) -> None:
-        """Function that calls get moves"""
-        # Clear each time otherwise we end up with duplicates
-        self.white_moves.clear()
-        self.black_moves.clear()
-        index: Tuple[int, int]
-        chess_square: str
-        piece_color: str
-        piece_type: str
-        # Loop board and get moves for each pieace
-        for index, chess_square in np.ndenumerate(self.board):  # type: ignore
-            if chess_square != "--":
+    def piece_movemovents(self) -> dict:
+        """Piece movements helper function"""
+        # pylint: disable=no-self-use
+        # fmt: off
+        map_dict = {
+            "P": {"movements": (1, -1), "continous": False},
+            "R": {"movements": [(1, 0), (0, 1), (-1, 0), (0, -1)], "continous": True},
+            "N": {"movements": [(-2, -1),(-2, 1),(2, -1),(2, 1),(-1, -2),(1, -2),(-1, 2),(1, 2),],"continous": False},
+            "B": {"movements": [(1, 1), (-1, 1), (1, -1), (-1, -1)], "continous": True},
+            "Q": {"movements": [(1, 1),(-1, 1),(1, -1),(-1, -1),(1, 0),(0, 1),(-1, 0),(0, -1),],"continous": True},
+            "K": {"movements": [(1, 1),(-1, 1),(1, -1),(-1, -1),(1, 0),(0, 1),(-1, 0),(0, -1),],"continous": False}
+        }
+        # fmt: on
+        return map_dict
 
-                array: list = []
-                piece_color, piece_type = chess_square  # type: ignore
-                array = self.white_moves if piece_color == "w" else self.black_moves
+    def get_piece_moves_dict(self, piece_type: str) -> Tuple[List[Tuple[int, int]], bool]:
+        """Return info: (dict) on ghow a particular piece moves"""
 
-                if piece_type == "P":  # Pawn
-                    self.get_pawn_moves(index, array, chess_square)
-                else:
-                    self.get_non_pawn_moves(index, array, chess_square)
-
-    def filter_invalid_moves(self) -> None:
-        """Remove illegal moves"""
-        black_invalid = []  # This will hold in valid moves
-        white_invalid = []
-
-        if self.player_turn == "white":
-            for white_move in self.white_moves:
-                self.make_move(white_move)
-                self.generate_all_moves()
-                for opponent_move in self.black_moves:
-                    white_king_location = self.get_king_location("wK")
-                    if white_king_location in opponent_move:
-                        white_invalid.append(white_move)
-                        break
-                self.undo_move()
-                self.generate_all_moves()
-        else:
-            for black_move in self.black_moves:
-                self.make_move(black_move)
-                self.generate_all_moves()
-                for opponent_move in self.white_moves:
-                    black_king_location = self.get_king_location("bK")
-                    if black_king_location in opponent_move:
-                        black_invalid.append(black_move)
-                        break
-                self.undo_move()
-                self.generate_all_moves()
-
-        self.black_moves = list(set(self.black_moves) - set(black_invalid))
-        self.white_moves = list(set(self.white_moves) - set(white_invalid))
-
-    def get_non_pawn_moves(self, index: Tuple[int, int], array: list, chess_square: str) -> None:
-        """Generate non-pawn moves here"""
-        # ---------------
-        row: int
-        col: int
-        piece_color: str
-        piece_type: str
-        row, col = index
-        piece_color, piece_type = chess_square  # type: ignore
-        # -------------------------------------
-        movements, is_continious = self.get_piece_moves_dict(piece_type)
-
-        for add_x, add_y in movements:  # Loop through piece movements list
-            new_row, new_col = row + add_x, col + add_y  # Get new starting pos
-            while self.is_in_bounds(new_row, new_col):
-
-                # Check if the square is empty
-                if self.board[new_row][new_col] == "--":
-                    array.append(f"{col}{row}:{new_col}{new_row}:N")
-                    if not is_continious:  # If piece type doesn't continuously move e.g Knight, Pawn, King etc..
-                        break
-                    new_row += add_x
-                    new_col += add_y
-                else:
-                    # Collides with team piece
-                    if self.board[new_row][new_col][0] == piece_color:
-                        break
-                    # Collides with enemy piece
-                    array.append(f"{col}{row}:{new_col}{new_row}:T")
-                    break
-
-    def get_pawn_moves(self, index: Tuple[int, int], array: list, chess_square: str) -> None:
-        """Generate pawn moves"""
-        # -------------------------------------
-        row: int
-        col: int
-        direction: int
-        piece_color: str
-        piece_type: str
-        row, col = index
-        piece_color, piece_type = chess_square  # type: ignore
-        direction = -1 if piece_color == "w" else 1
-        # -------------------------------------
-        movements, _ = self.get_piece_moves_dict(piece_type)
-
-        # Check if its inbounds
-        if self.is_in_bounds(row + direction, col):
-
-            # One square move
-            if self.board[row + direction][col] == "--":  # If empty
-                array.append(f"{col}{row}:{col}{row + direction}:N")
-
-                # Two square move
-                if (
-                    not self.has_pawn_moved(row, piece_color) and self.board[row + (direction * 2)][col] == "--"
-                ):  # If its empty and pawn hasn't moved
-                    array.append(f"{col}{row}:{col}{row+(direction*2)}:N")
-
-            # Captures
-            for add_y in movements:
-                new_y = col + add_y  # type: ignore
-                if 0 <= new_y <= 7:  # In-bounds
-                    if self.board[row + direction][new_y][0] != "-":  # Not empty square
-                        if self.board[row + direction][new_y][0] != piece_color:  # Collides with enemy
-                            array.append(f"{col}{row}:{new_y}{row+direction}:T")
+        piece_move_info: dict = self.piece_moves[piece_type]
+        movements: List[Tuple[int, int]] = piece_move_info["movements"]
+        continuous: bool = piece_move_info["continous"]
+        return movements, continuous
